6b0fb1d0acc4f2da58ee114a9a7b859ca165d15a
Joel Grayson <joel@joelgrayson.com>
2022-10-06 14:22:46 -0400

    stack and gitignore

---
+++ 5_stack/stack.s
@@ -0,0 +1,16 @@
+.global _start
+_start:
+	LDR R0, =list
+	LDR R1, [R0]
+	LDR R2, [R0, #4]
+	LDR R3, [R0, #8]
+	LDR R4, [R0, #0xC]
+	LDR R5, [R0, #0xE]
+
+
+.data
+hello:
+	.ascii "Joel Grayson"
+list:
+	.word 1, 2, 4, -8
+
